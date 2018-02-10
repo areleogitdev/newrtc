b9e6c3e4fcc5653e9dd221f19bf6b91f9aade0ae
areleogitdev <n0w.a7ch@gmail.com>
2018-02-10 16:03:28 +0700

    first cZXhZhZnxnnnxxzn   ndashdshsdax

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 });
 
+app.get('/index',function(request,response){
+
+		response.sendfile("index.html");
+
+    
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
