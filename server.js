31ce85785489d68752dec2c8dadd4e0c2aa10602
areleogitdev <n0w.a7ch@gmail.com>
2018-02-10 12:19:36 +0700

    first dhhsdf sdusdf7sd8isd

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
